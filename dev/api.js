859c5f1e89555ee38b83caeef412f5ba9f088676
lk <lucaskeys@gmail.com>
2020-08-28 09:46:59 -0700

    Add postpost andget request functions

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const Blockchain = require('./blockchain')
+
+const bitcoin = new Blockchain();
+// if request comes in with JSON data, parse the data and access in any of these routes
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/blockchain', function (req, res) {
+  res.send(bitcoin)
+})
+
+app.post('/transaction', function(req, res) {
+  console.log(req.body)
+  const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient)
+  res.json({note: `Transaction will be added in block ${blockIndex}`})
+  
+})
+
+app.get('/mine', function(res) {
+
 })
  
-app.listen(3000)
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
